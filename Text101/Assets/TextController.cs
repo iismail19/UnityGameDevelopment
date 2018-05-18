d5ab6d6fa850501669c1794d2933365ae4ca23c1
ismail ismail <iismail1997@github.com>
2018-05-18 15:10:08 -0500

    added fucntions for each state to Text101

--- Text101/Assets/TextController.cs
+++ Text101/Assets/TextController.cs
@@ -104,4 +104,91 @@ public class TextController : MonoBehaviour {
 			        "Press P to Play again";
 		if (Input.GetKeyDown(KeyCode.P))        { myState = States.cell; }
 	}
+
+	void in_closet()
+    {
+        text.text = "Inside the closet you see a cleaner's uniform that looks about your size! " +
+                    "Seems like your day is looking-up.\n\n" +
+                    "Press D to Dress up, or R to Return to the corridor";
+        if (Input.GetKeyDown(KeyCode.R)) { myState = States.corridor_2; }
+        else if (Input.GetKeyDown(KeyCode.D)) { myState = States.corridor_3; }
+    }
+
+    void closet_door()
+    {
+        text.text = "You are looking at a closet door, unfortunately it's locked. " +
+                    "Maybe you could find something around to help enourage it open?\n\n" +
+                    "Press R to Return to the corridor";
+        if (Input.GetKeyDown(KeyCode.R)) { myState = States.corridor_0; }
+    }
+
+    void corridor_3()
+    {
+        text.text = "You're standing back in the corridor, now convincingly dressed as a cleaner. " +
+                    "You strongly consider the run for freedom.\n\n" +
+                    "Press S to take the Stairs, or U to Undress";
+        if (Input.GetKeyDown(KeyCode.S)) { myState = States.courtyard; }
+        else if (Input.GetKeyDown(KeyCode.U)) { myState = States.in_closet; }
+    }
+
+    void corridor_2()
+    {
+        text.text = "Back in the corridor, having declined to dress-up as a cleaner.\n\n" +
+                    "Press C to revisit the Closet, and S to climb the stairs";
+        if (Input.GetKeyDown(KeyCode.C)) { myState = States.in_closet; }
+        else if (Input.GetKeyDown(KeyCode.S)) { myState = States.stairs_2; }
+    }
+
+    void corridor_1()
+    {
+        text.text = "Still in the corridor. Floor still dirty. Hairclip in hand. " +
+                    "Now what? You wonder if that lock on the closet would succumb to " +
+                    "to some lock-picking?\n\n" +
+                    "P to Pick the lock, and S to climb the stairs";
+        if (Input.GetKeyDown(KeyCode.P)) { myState = States.in_closet; }
+        else if (Input.GetKeyDown(KeyCode.S)) { myState = States.stairs_1; }
+    }
+
+    void floor()
+    {
+        text.text = "Rummagaing around on the dirty floor, you find a hairclip.\n\n" +
+                    "Press R to Return to the standing, or H to take the Hairclip.";
+        if (Input.GetKeyDown(KeyCode.R)) { myState = States.corridor_0; }
+        else if (Input.GetKeyDown(KeyCode.H)) { myState = States.corridor_1; }
+    }
+
+    void courtyard()
+    {
+        text.text = "You walk through the courtyard dressed as a cleaner. " +
+                    "The guard tips his hat at you as you waltz past, claiming " +
+                    "your freedom. You heart races as you walk into the sunset.\n\n" +
+                    "Press P to Play again.";
+        if (Input.GetKeyDown(KeyCode.P)) { myState = States.cell; }
+    }
+
+    void stairs_0()
+    {
+        text.text = "You start walking up the stairs towards the outside light. " +
+                    "You realise it's not break time, and you'll be caught immediately. " +
+                    "You slither back down the stairs and reconsider.\n\n" +
+                    "Press R to Return to the corridor.";
+        if (Input.GetKeyDown(KeyCode.R)) { myState = States.corridor_0; }
+    }
+
+    void stairs_1()
+    {
+        text.text = "Unfortunately weilding a puny hairclip hasn't given you the " +
+                    "confidence to walk out into a courtyard surrounded by armed guards!\n\n" +
+                    "Press R to Retreat down the stairs";
+        if (Input.GetKeyDown(KeyCode.R)) { myState = States.corridor_1; }
+    }
+
+    void stairs_2()
+    {
+        text.text = "You feel smug for picking the closet door open, and are still armed with " +
+                    "a hairclip (now badly bent). Even these achievements together don't give " +
+                    "you the courage to climb up the staris to your death!\n\n" +
+                    "Press R to Return to the corridor";
+        if (Input.GetKeyDown(KeyCode.R)) { myState = States.corridor_2; }
+    }
 }

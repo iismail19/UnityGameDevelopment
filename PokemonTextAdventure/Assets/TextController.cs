2130a0c482391f68a21102e5176ee19e58745b92
ismail ismail <iismail1997@github.com>
2018-05-20 10:35:40 -0500

    Set up Pokemon Project game UI

---
+++ PokemonTextAdventure/Assets/TextController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+	public Text mytext; 
+	private String pokemon_name;
+	private int pokemon_level;
+
+
+	// Use this for initialization
+	void Start () {
+		mytext.text = "Welcome to pokemon World";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

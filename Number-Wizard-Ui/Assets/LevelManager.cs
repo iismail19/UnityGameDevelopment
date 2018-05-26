ffc3a628498f63449bb6c45fe51b3cfad79734a4
ismail ismail <iismail1997@github.com>
2018-05-26 13:16:34 -0500

    added level manager

---
+++ Number-Wizard-Ui/Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("Level load requested " + name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log("Requesting Quit Game");
+	}
+}

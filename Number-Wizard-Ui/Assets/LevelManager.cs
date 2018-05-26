603d3276d068842bddeca055e51eb3fea155462a
ismail ismail <iismail1997@github.com>
2018-05-26 17:04:08 -0500

    Added the number wizard script

--- Number-Wizard-Ui/Assets/LevelManager.cs
+++ Number-Wizard-Ui/Assets/LevelManager.cs
@@ -6,9 +6,14 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name){
 		Debug.Log("Level load requested " + name);
+		Application.LoadLevel(name); // this will load the specified scence/level
 	}
 
+	// Application.Quit() does not work in web-build or debugg mode, works great for console & PC/Mac/Linux
+    // for app dev dont quit the application your self, have user or OS manage applicaitons
 	public void QuitRequest(){
 		Debug.Log("Requesting Quit Game");
+		Application.Quit();
+
 	}
 }
---
+++ Number-Wizard-Ui/Assets/Scenes/NumberWIzard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWIzard : MonoBehaviour {
+
+	int max;
+    int min;
+	int guess;
+	public int maxGuessesAllowed = 5;
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		StartGame();
+	}
+	   
+	void StartGame(){
+		max = 1000;
+		min = 500;
+		guess = 500;
+
+		max = max + 1; // bc if your number is 1000, (min + max) / 2 rounds down to 999.
+	}
+
+	public void GuessHigher(){
+		min = guess;
+		NextGuess();
+	}
+
+	public void GuessLower(){
+		max = guess;
+        NextGuess();
+	}
+
+	void NextGuess(){
+		guess = (max + min) / 2;
+		text.text = guess.ToString();
+		maxGuessesAllowed--;
+		if(maxGuessesAllowed <= 0){
+			Application.LoadLevel("Win");
+		}
+	}
+
+}

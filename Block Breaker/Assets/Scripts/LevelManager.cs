f7b3f3934edc9d5f79d516b83ad571ccc0f8ee80
ismail ismail <iismail1997@github.com>
2018-05-31 15:48:21 -0500

    Added Scenes as imported from pm previous project, but will be revised to fit this game

---
+++ Block Breaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("Level load requested " + name);
+		Application.LoadLevel(name); // this will load the specified scence/level
+	}
+
+	// Application.Quit() does not work in web-build or debugg mode, works great for console & PC/Mac/Linux
+    // for app dev dont quit the application your self, have user or OS manage applicaitons
+	public void QuitRequest(){
+		Debug.Log("Requesting Quit Game");
+		Application.Quit();
+
+	}
+}
